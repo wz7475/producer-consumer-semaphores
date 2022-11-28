b822d03fc1c4aebbf0242b735d4d2e960f75132d
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2022-11-28 16:23:27 +0100

    add brackets to start

--- root_cp/start.c
+++ root_cp/start.c
@@ -6,9 +6,10 @@
 int main(void) {
    int i;
    pid_t child_pid;
-   for(i = 0; i < 2; i++)
+   for(i = 0; i < 2; i++){
      child_pid = fork();
-   if(child_pid == 0)
-     execvp("./test", NULL);
+     if(child_pid == 0)
+       execvp("./test", NULL);
+   }
    return 0;
 }
